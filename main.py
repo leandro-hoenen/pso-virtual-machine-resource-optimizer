65acf303e5f366cee3b643b676603b00424da2ca
leandro-hoenen <exponent_stooge805@perfunc.ch>
2024-09-17 15:29:19 +0200

    integrated normalization in existing algorithm

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-from matplotlib import pyplot as plt
 import numpy as np
 import pyswarms as ps
-from pyswarms.utils.plotters import plot_cost_history
+from matplotlib import pyplot as plt
 from py4j.java_gateway import JavaGateway
+from pyswarms.utils.plotters import plot_cost_history
 
 
 def linear_normalizer(x, min_of_x, range_of_x):
@@ -12,9 +11,7 @@ def linear_normalizer(x, min_of_x, range_of_x):
     return normalized_value
 
 
-def resource_normalizer(min_ram_in_mb, max_ram_in_mb, current_ram_in_mb, min_vcpus, max_vcpus, current_vcpus):
-    max_resources = (max_ram_in_mb / 1000) + max_vcpus
-    min_resources = (min_ram_in_mb / 1000) + min_vcpus
+def resource_normalizer(current_ram_in_mb, current_vcpus):
     range_resources = max_resources - min_resources
     resources = (current_ram_in_mb / 1000) + current_vcpus
     resources_normalized = linear_normalizer(resources, min_resources, range_resources)
@@ -22,26 +19,13 @@ def resource_normalizer(min_ram_in_mb, max_ram_in_mb, current_ram_in_mb, min_vcp
     return resources_normalized
 
 
-def execution_time_normalizer(min_ram_in_mb, max_ram_in_mb, min_vcpus, max_vcpus, current_vcpus, current_ram_in_mb):
-    max_execution_time = cloud_sim_runner.runSimulation(min_vcpus, min_ram_in_mb)
-    min_execution_time = cloud_sim_runner.runSimulation(max_vcpus, max_ram_in_mb)
+def execution_time_normalizer(current_vcpus, current_ram_in_mb):
     range_of_execution_time = max_execution_time - min_execution_time
     execution_time = cloud_sim_runner.runSimulation(current_vcpus, current_ram_in_mb)
     execution_time_normalized = linear_normalizer(execution_time, min_execution_time, range_of_execution_time)
 
     return execution_time_normalized
 
-
-gateway = JavaGateway()
-
-cloud_sim_runner = gateway.entry_point.getCloudSimRunner()
-min_vcpus = 1
-min_ram_in_mb = 1
-max_bound = 20
-max_ram_in_mb = max_bound*1000
-max_vcpus = max_bound
-
-max_execution_time = cloud_sim_runner.runSimulation(min_vcpus, min_ram_in_mb)
 
 def fitness_function(x):
     fitness = []
@@ -59,14 +43,30 @@ def fitness_function(x):
             execution_time = np.inf
 
         # Normalize execution time
-        normalized_execution_time = execution_time / math.sqrt(max_execution_time)
-        # Calculate total resource usage
-        total_resource_usage = cpus + ram / 1000
-        fitness_value = (1.7 * normalized_execution_time) + (0.3 * total_resource_usage)
+        normalized_execution_time = execution_time_normalizer(cpus, ram)
+        # Compute and normalize resource usage
+        normalized_resource_usage = resource_normalizer(ram, cpus)
+        fitness_value = (1.7*normalized_execution_time) + (0.3*normalized_resource_usage)
         fitness.append(fitness_value)
 
     return np.array(fitness)
 
+
+# Load CloudSim Plus simulation enviroment
+gateway = JavaGateway()
+cloud_sim_runner = gateway.entry_point.getCloudSimRunner()
+
+# Normalization parameters
+min_vcpus = 1
+min_ram_in_mb = 1000
+max_ram_in_mb = 20000
+max_vcpus = 20
+# Execution time
+max_execution_time = cloud_sim_runner.runSimulation(min_vcpus, min_ram_in_mb)
+min_execution_time = cloud_sim_runner.runSimulation(max_vcpus, max_ram_in_mb)
+# Resources
+max_resources = (max_ram_in_mb / 1000) + max_vcpus
+min_resources = (min_ram_in_mb / 1000) + min_vcpus
 
 # Define the bounds for vmPes and ram
 options = {'c1': 0.5, 'c2': 0.5, 'w': 0.9}
@@ -78,10 +78,10 @@ bounds = (min_bound, max_bound)
 optimizer = ps.single.GlobalBestPSO(n_particles=120, dimensions=2, options=options, bounds=bounds)
 
 # Perform the optimization
-cost, pos = optimizer.optimize(fitness_function, iters=200)
+cost, pos = optimizer.optimize(fitness_function, iters=100)
 gateway.close()
 optimal_cpus = round(pos[0])
-optimal_ram = round(pos[1])*1000
+optimal_ram = round(pos[1]*1000)
 
 # Run the simulation with the optimal configuration to get the execution time
 try:
